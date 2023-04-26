7012c94263403c5b3fc0c49ad4c1244bf4c78d04
DominicLin1 <dominiclin777@gmail.com>
2023-04-26 10:11:44 -0400

    ffdsa3

--- NYC-API/src/views/HomeView.vue
+++ NYC-API/src/views/HomeView.vue
@@ -6,18 +6,20 @@ import BarChart from '../components/Chart/ChartTemplate.vue'
 
 
   <template>
-  <BarChart />
+  <BarChart></BarChart>
 </template>
 
 <script>
 import BarChart from 'path/to/component/BarChart'
+import TheWelcome from '../components/TheWelcome.vue'
 
 export default {
   name: 'App',
   components: { BarChart }
 }
 </script>
+<template>
   <main>
-    <TheWelcome />
+    <TheWelcome></TheWelcome>
   </main>
 </template>
