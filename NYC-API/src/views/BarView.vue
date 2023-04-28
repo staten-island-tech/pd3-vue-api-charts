5e00b200becc71c4bff9208786b88935ea694735
LeoDwi <leod13579@gmail.com>
2023-04-28 09:30:28 -0400

    screwed with charts

--- NYC-API/src/views/BarView.vue
+++ NYC-API/src/views/BarView.vue
@@ -3,12 +3,16 @@ import BarChart from "../components/Charts/BarChart.vue";
 import { ref, onMounted } from "vue";
 const dogs = ref("");
 async function getData() {
+  this.retrievedData = false;
   try {
     let data = await fetch(
       "https://data.cityofnewyork.us/resource/nu7n-tubp.json"
     );
-    let object = data.json();
-    dogs.value = object.results;
+    console.log(data);
+    let object = await data.json();
+    console.log(object);
+    dogs.value = object;
+    console.log(dogs);
     this.retrievedData = true;
   } catch (error) {
     console.log(error);
@@ -21,7 +25,7 @@ onMounted(() => {
 
 <template>
   <div class="bar">
-    <BarChart :chartData="dogs" />
+    <BarChart v-if="retrievedData" :chartData="dogs" />
     <h1>hi</h1>
   </div>
 </template>
--- NYC-API/src/views/PieView.vue
+++ NYC-API/src/views/PieView.vue
@@ -1,3 +1,27 @@
-<script setup>
+<script>
 import PieChart from "../components/Charts/PieChart.vue";
+export default {
+  components: {
+    pieChart,
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [male, female],
+        datasets: [2, 3],
+      },
+      chartOptions: {
+        responsive: true,
+      },
+    };
+  },
+  methods: {
+    getData: async function () {
+      let data = await fetch(
+        "https://data.cityofnewyork.us/resource/nu7n-tubp.json"
+      );
+      let object = await data.json();
+    },
+  },
+};
 </script>
