46373e426f14efa67e1b9caca4acc5fe1b58fac8
DominicLin1 <113190169+DominicLin1@users.noreply.github.com>
2023-04-30 17:39:42 -0400

    downloaded aot

--- NYC-API/src/main.js
+++ NYC-API/src/main.js
@@ -6,7 +6,11 @@ import "./assets/main.css";
 
 
 
-
+import "./assets/main.css";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const app = createApp(App);
 
