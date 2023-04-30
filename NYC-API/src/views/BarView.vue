271464f6913d3f91d67037fd936ed07c08e2d5a7
DominicLin1 <113190169+DominicLin1@users.noreply.github.com>
2023-04-30 17:44:29 -0400

    abort mission

--- NYC-API/src/views/BarView.vue
+++ NYC-API/src/views/BarView.vue
@@ -1,5 +1,15 @@
 <script>
+<<<<<<< HEAD
 import BarChart from "../components/Charts/BarChart.vue";
+=======
+import BarChart from '../components/Chart/ChartTemplate.vue'
+import TheWelcome from '../components/TheWelcome.vue'
+
+export default {
+  name: 'App',
+  components: { BarChart }
+}
+>>>>>>> parent of 8e2042d (sanmdas,m)
 </script>
 
 <template>
