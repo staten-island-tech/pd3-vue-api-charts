3da400414d14be2a29f075ca0d27b93f318b0018
DominicLin1 <113190169+DominicLin1@users.noreply.github.com>
2023-04-30 17:49:26 -0400

    RECOVEER MISSION SUCESSFUL
    DAKLNSD

--- NYC-API/src/main.js
+++ NYC-API/src/main.js
@@ -2,20 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 
 
 import "./assets/main.css";
-=======
->>>>>>> parent of ba43d25 (Merge pull request #13 from staten-island-tech/dominic's-up-to-date-branch)
-=======
->>>>>>> parent of 8e2042d (sanmdas,m)
-=======
->>>>>>> parent of ba43d25 (Merge pull request #13 from staten-island-tech/dominic's-up-to-date-branch)
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
--- NYC-API/src/views/BarView.vue
+++ NYC-API/src/views/BarView.vue
@@ -1,25 +1,28 @@
+
+
 <script>
-<<<<<<< HEAD
-<<<<<<< HEAD
-import BarChart from "../components/Charts/BarChart.vue";
-=======
-import BarChart from '../components/Chart/ChartTemplate.vue'
-import TheWelcome from '../components/TheWelcome.vue'
+import BarChart from '../components/Charts/ChartTemplate.vue'
+
 
 export default {
   name: 'App',
   components: { BarChart }
 }
->>>>>>> parent of 8e2042d (sanmdas,m)
-=======
-import BarChart from "../components/Charts/BarChart.vue";
->>>>>>> parent of ba43d25 (Merge pull request #13 from staten-island-tech/dominic's-up-to-date-branch)
 </script>
-
 <template>
-  <div class="bar">
-    <RouterLink to="/pie"></RouterLink>
-    <BarChart v-if="retrievedData" :chartData="dogs" />
-    <h1>hi</h1>
-  </div>
+  <main>
+    <div class="Bar">
+    <BarChart>Birthyear</BarChart>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.Bar{
+  width: 100%;
+  background-color:darkgray;
+  
+  
+}
+</style>
+
