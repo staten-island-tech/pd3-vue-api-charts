b17738d8b1ee096e91d9e1c07b4a377ba0b6d731
DominicLin1 <113190169+DominicLin1@users.noreply.github.com>
2023-04-30 16:16:10 -0400

    D,MQM;L,D

--- NYC-API/src/components/Chart/ChartTemplate.vue
+++ NYC-API/src/components/Chart/ChartTemplate.vue
@@ -1,6 +1,6 @@
 <template>
     <Bar
-      v-if="loaded"
+      v-if="retrievedData"
       id="my-chart-id"
       :options="chartOptions"
       :data="chartData"
@@ -18,67 +18,54 @@
   export default {
     name: 'BarChart',
     components: { Bar },
-    data() {
-      return {
-       
-        chartData: {
-          labels: [],
-          datasets: []
-        },
-        chartOptions: {
-          responsive: true
-        },
-            loaded: false,
-      }
-      
-    },
-    
-  
-mounted() {
-    this.getCD();
- 
+     data() {
+    return {
+      chartData: {
+        labels: [],
+        datasets: [{ data: [] }],
+      },
+      chartOptions: {
+        responsive: true,
+        backgroundColor: ["#000fff"],
+      },
+      retrievedData: false,
+    };
+  },
+  mounted() {
+    this.getChartData();
   },
   methods: {
- 
-   getCD: async function  () {
-      
- let dates = [];
-  
-      let label = [];
-      
-      this.loaded = false;
+    getChartData: async function () {
+      this.retrievedData = false;
+      let years = [];
+      let time = [];
       try {
-        let api = await fetch(
+        let data = await fetch(
           "https://data.cityofnewyork.us/resource/nu7n-tubp.json"
         );
-        let object=await api.json()
-       console.log(dates)
-        object.forEach((dog=>{
-          let birth=dog.animalbirth
-          dates.push(birth)
-         
-        }))
-    let x= new Set(dates)
-    x.forEach((birth)=>{
-      label.push(birth)
-    })
-    this.loaded = true;
-    console.log(x)
-this.chartData.datasets.push(dates)
-         this.chartData.labels.push(label)
-      }
-        
-      
-      catch (error) {
+        let object = await data.json();
+        let allYears = [];
+        object.forEach((dog) => {
+          allYears.push(dog.animalbirth);
+        });
+        years = new Set(allYears);
+        years.forEach((year) => {
+          time.push(year);
+        });
+        let finalYears = time.sort();
+        this.chartData.labels = finalYears;
+        finalYears.forEach((year) => {
+          let count = this.filter(object, year);
+          this.chartData.datasets[0].data.push(count.length);
+        });
+        this.retrievedData = true;
+      } catch (error) {
         console.log(error);
       }
-       
+    },
+    filter: function (object, year) {
+      return object.filter((dog) => dog.animalbirth === year);
+    },
   },
-  
-
-  }
-  }
-  
-  
-  
-  </script>
+};
+</script>
