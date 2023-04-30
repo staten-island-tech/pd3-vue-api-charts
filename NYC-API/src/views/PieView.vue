f606d8e6e81d7b44770619783960e5a4c1152d14
LeoDwi <leod13579@gmail.com>
2023-04-29 21:39:16 -0400

    additional feature for pie chart

--- NYC-API/src/views/PieView.vue
+++ NYC-API/src/views/PieView.vue
@@ -8,7 +8,7 @@ export default {
   data() {
     return {
       chartData: {
-        labels: ["Male", "Female"],
+        labels: [],
         datasets: [{ data: [] }],
       },
       chartOptions: {
@@ -22,6 +22,13 @@ export default {
     this.getChartData();
   },
   methods: {
+    clearChartData: function () {
+      this.retrievedData = false;
+      this.chartData.datasets[0].data.length = 0;
+      this.chartData.labels.length = 0;
+      console.log(this.chartData.datasets[0].data.length);
+      console.log(this.chartData.labels.length);
+    },
     getChartData: async function () {
       this.retrievedData = false;
       try {
@@ -30,13 +37,57 @@ export default {
         );
         let object = await data.json();
         let male = object.filter((dog) => dog.animalgender === "M");
+        this.chartData.labels.push("Male");
         this.chartData.datasets[0].data.push(male.length);
         let female = object.filter((dog) => dog.animalgender === "F");
+        this.chartData.labels.push("Female");
         this.chartData.datasets[0].data.push(female.length);
         this.retrievedData = true;
       } catch (error) {
         console.log(error);
       }
+    },
+    getLiscenseData: async function () {
+      this.retrievedData = false;
+      let dates = [];
+      let labels = [];
+      try {
+        let data = await fetch(
+          "https://data.cityofnewyork.us/resource/nu7n-tubp.json"
+        );
+        let object = await data.json();
+        object.forEach((dog) => {
+          let year = dog.licenseissueddate.substring(0, 7);
+          dates.push(year);
+        });
+        let noDupes = new Set(dates);
+        noDupes.forEach((month) => {
+          labels.push(month);
+        });
+        let ordered = labels.sort();
+        this.filter(september, 0);
+        this.filter(october, 1);
+        this.filter(november, 2);
+        this.filter(december, 3);
+        this.chartData.labels = ordered;
+        this.retrievedData = true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    filter: function (month, index) {
+      let month = object.filter(
+        (dog) => dog.licenseissueddate.substring(0, 7) === ordered[index]
+      );
+      this.chartData.datasets[0].data.push(month.length);
+    },
+    years: function () {
+      this.clearChartData();
+      this.getLiscenseData();
+    },
+    gender: function () {
+      this.clearChartData();
+      this.getChartData();
     },
   },
 };
@@ -44,7 +95,8 @@ export default {
 
 <template>
   <div class="pie">
-    <button class="test" v-on:click="getChartData">eeeeeeeee</button>
+    <button class="test" v-on:click="years">go</button>
+    <button class="button" v-on:click="gender">go</button>
     <PieChart
       v-if="retrievedData"
       :chartData="chartData"
@@ -55,6 +107,6 @@ export default {
 
 <style>
 .test {
-  font-size: 100px;
+  font-size: 20px;
 }
 </style>
