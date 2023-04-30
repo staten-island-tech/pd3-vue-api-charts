0265d39f24a834598d7ce6383b39b70321aa7d8e
LeoDwi <leod13579@gmail.com>
2023-04-30 14:32:51 -0400

    Major css changes

--- NYC-API/src/views/AboutView.vue
+++ NYC-API/src/views/AboutView.vue
@@ -2,16 +2,32 @@
 
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 data-aos="zoom-in-down" data-aos-duration="3000">About the devs:</h1>
+    <p data-aos="zoom-in-up" data-aos-duration="3000">
+      This Vue chart project was made by those two idiots, Leo and Dominic. As
+      Whalen likes to call them, they are the two old white muppets from The
+      Muppets. In this project, you will find only 2 different charts and 3
+      datasets because that was all we needed to do. Our budget was running low
+      so that will be all you get from us. ¯\_(ツ)_/¯
+    </p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  text-align: center;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+}
+h1 {
+  margin: 50px, 0px;
+  font-size: 40px;
+}
+p {
+  font-size: 20px;
+  width: 50vw;
+  margin: auto;
 }
 </style>
--- NYC-API/src/views/HomeView.vue
+++ NYC-API/src/views/HomeView.vue
@@ -1,5 +1,56 @@
-<script setup></script>
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <h1 data-aos="zoom-in-down" data-aos-duration="3000">
+      NYC dog license data charts here:
+    </h1>
+    <header>
+      <nav data-aos="zoom-in-up" data-aos-duration="3000">
+        <RouterLink to="/">Home:</RouterLink>
+        <RouterLink to="/about">About:</RouterLink>
+        <RouterLink to="/bar">Bar Chart:</RouterLink>
+        <RouterLink to="/pie">Pie Chart:</RouterLink>
+      </nav>
+    </header>
+  </main>
 </template>
+
+<style>
+h1 {
+  font-size: 40px;
+  text-align: center;
+  margin: 40px 0px;
+}
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+nav {
+  width: 100%;
+  font-size: 30px;
+  text-align: center;
+  margin-top: 1rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
